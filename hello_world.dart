c322e1baa9fb5bd41cd99a805bb13939b7d6e1fb
Michael <msousacode@gmail.com>
2023-03-21 21:36:27 -0300

    feat: implementation use variables.

--- hello_world.dart
+++ hello_world.dart
@@ -1,3 +1,11 @@
-void main(){
-    print('Hello word');
+var firstVariable;
+var secondVariable;
+
+void main() {
+  firstVariable = "Argentina";
+  secondVariable = "Brasil";
+
+  print(firstVariable);
+  print("-----------");
+  print(secondVariable);
 }
