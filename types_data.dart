91352ea1b7e6b6ae3039e810600bfa4b771ff2e7
Michael <msousacode@gmail.com>
2023-03-21 21:41:58 -0300

    feat: learning boolean type

---
+++ types_data.dart
@@ -0,0 +1,16 @@
+void main() {
+  bool typeBoolean;
+
+  typeBoolean = false;
+
+  final fruty1 = "apple";
+  final fruty2 = "apple";
+
+  if (fruty1 == fruty2) {
+    typeBoolean = true;
+  } else {
+    typeBoolean = false;
+  }
+
+  print(typeBoolean);
+}
