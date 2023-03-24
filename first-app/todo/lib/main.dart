0ab1042c8016e79dd10ada7dcf50e02cf4f34174
Michael <msousacode@gmail.com>
2023-03-24 15:13:30 -0300

    feat: working with ListView

--- first-app/todo/lib/main.dart
+++ first-app/todo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/models/item.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,14 +21,35 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  //agora pode gerenciar estado das propriedades quando transformado de Statelees para Stateful.
+
+  var items = <Item>[];
+
+  HomePage({super.key}) {
+    items = [];
+    items.add(Item(title: "title1", done: true));
+    items.add(Item(title: "title2", done: true));
+    items.add(Item(title: "title3", done: true));
+  }
+
   @override
-  Widget build(Object context) {
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("To List"),
       ),
-      body: const Center(child: Text('Olá Brasil')),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext contexts, int index) {
+          return Text(widget.items[index].title);
+        },
+      ),
     );
   }
 }
