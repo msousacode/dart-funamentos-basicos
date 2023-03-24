300cfc9a68a52f703414938e9f3426480cb2f016
Michael <msousacode@gmail.com>
2023-03-24 14:55:51 -0300

    feat: create scaffold

---
+++ first-app/todo/lib/models/item.dart
@@ -0,0 +1,18 @@
+class Item {
+  late String title;
+  late bool done;
+
+  Item({required this.title, required this.done});
+
+  Item.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    done = json['done'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['title'] = title;
+    data['done'] = done;
+    return data;
+  }
+}
