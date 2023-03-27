5d75ea2a55a5cd714f19848585e60bf8ccd1dba5
Michael <msousacode@gmail.com>
2023-03-27 18:36:47 -0300

    feat: added functions add item e remove item

--- first-app/todo/lib/main.dart
+++ first-app/todo/lib/main.dart
@@ -38,27 +38,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //prepara uma propriedade para receber os dados digitados na tela.
+  var task = TextEditingController();
+
+  //Cria uma acao para o botao.
+  void add() {
+    setState(() {
+      widget.items.add(
+        Item(title: task.text, done: false),
+      );
+      task.text = "";
+    });
+  }
+
+  //Cria a acao de remover
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("To List"),
+        title: TextFormField(
+          controller: task, //Faz o binding do input que recebe os dados.
+          keyboardType: TextInputType.text, //Exibe o teclado de e-mail
+          style: const TextStyle(color: Colors.amber, fontSize: 24),
+        ),
       ),
       body: ListView.builder(
         itemCount: widget.items.length,
         itemBuilder: (BuildContext contexts, int index) {
           final item = widget.items[index];
 
-          return CheckboxListTile(
-              title: Text(item.title),
+          return Dismissible(
+              //Adiciona um efeito de arrastar para a direita ou para a esquerda
               key: Key(item.title),
-              value: item.done,
-              onChanged: (value) {
-                setState(() {
-                  item.done = value;
-                });
-              });
+              background: Container(
+                color: Colors.red.withOpacity(0.2),
+                child: const Text("Excluir"),
+              ),
+              onDismissed: (direction) {
+                //Realiza a acao remove depois de terminar o arrastar.
+                remove(index);
+              },
+              child: CheckboxListTile(
+                  //é a lista de checkboxs
+                  title: Text(item.title),
+                  value: item.done,
+                  onChanged: (value) {
+                    setState(() {
+                      item.done = value;
+                    });
+                  }));
         },
+      ),
+      //Adiciona um botao na tela
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.phone),
+        backgroundColor: Colors.pink,
       ),
     );
   }
