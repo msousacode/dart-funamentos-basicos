d647f73ec3a28957e4e8c6502362648577ed4984
Michael <msousacode@gmail.com>
2023-03-28 21:58:13 -0300

    feat: uso do ListView, Column e Rows

--- dominando-layouts/layouts/lib/main.dart
+++ dominando-layouts/layouts/lib/main.dart
@@ -23,14 +23,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //É uma boa prácita usar o Scaffold pois ele vem com regras de layouts melhores que o Container. Vem com uma estrutura de aplicativo.
-      appBar: AppBar(
-        title: Text("meu app"),
-      ),
-      body: Container(
-        color: Colors.blue,
-        child: Center(child: Text("XXXXXX")),
-      ),
-    );
+        //É uma boa prácita usar o Scaffold pois ele vem com regras de layouts melhores que o Container. Vem com uma estrutura de aplicativo.
+        appBar: AppBar(
+          title: Text("meu app"),
+        ),
+        body: Container(
+          width: double.infinity, //oculpar a tela inteira
+          color: Colors.red,
+          child: ListView(
+            //ListView juntos com Column e Rows são muito utilizados para organizar os elementos na pagina do app, principalmente o ListView.
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              Container(
+                color: Colors.yellow,
+                child: Text("xxxxx"),
+              )
+            ],
+          ),
+
+          /*
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment
+                .spaceAround, //centraliza a coluna alinhamento na vertical
+            crossAxisAlignment: CrossAxisAlignment
+                .center, //centraliza a coluna alinhamento na horizontal
+            children: const <Widget>[
+              Text("Item 1"),
+              Text("Item 2"),
+              Text("Item 3")
+            ],
+          ),*/
+        ));
   }
 }
